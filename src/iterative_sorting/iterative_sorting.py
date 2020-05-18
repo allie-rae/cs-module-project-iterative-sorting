8ae20cfe1dade5036f85d2d47445a2a82310600b
Allie Robinson <allierob93@gmail.com>
2020-05-18 17:58:10 -0500

    mvp met

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,23 +2,42 @@
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+        smallest_index = i
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
-
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
-        # Your code here
-
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
+    # this swapped variable will stop the while loop
+    swapped = True
+    # this sorted variable keeps track of how many items
+    # have been sorted while the loop is running
+    sorted = -1
+    # this loop will continue until there are no swaps
+    while swapped:
+        # if swapped does not change to True, this will break
+        # the loop
+        swapped = False
+        # this increases by 1 through each loop because
+        # 1 item is sorted per loop
+        sorted += 1
+        # since we decided the first element in the array is already
+        # sorted, we begin with 1 instead of 0
+        # we end with len(arr)-sorted because we don't go through
+        # the already sorted indices each time
+        for i in range(1, len(arr)-sorted):
+            # if we find a smaller value while iterating through
+            # the loop, we swap them
+            if arr[i] < arr[i - 1]:
+                swapped = True
+                arr[i], arr[i - 1] = arr[i - 1], arr[i]
 
     return arr
 
@@ -26,6 +45,5 @@ def bubble_sort(arr):
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
     # Your code here
-
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,31 @@
+import math
+
+
 def linear_search(arr, target):
-    # Your code here
-
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
+    # grab middle of array
+    # check if middle is equal to, <, or > target
+    # if equal, stop
+    # if less than, pick the mid of the left half
+    # if greater than, pick the mid of the right half
+    # repeat until start crosses end
+    middle_index = math.ceil(len(arr)/2-1)
+    start_index = 0
+    end_index = len(arr) - 1
+    while start_index < end_index:
+        if arr[middle_index] == target:
+            return middle_index
+        elif arr[middle_index] > target:
+            end_index = middle_index - 1
+            middle_index = math.ceil((start_index + end_index) / 2)
+        elif arr[middle_index] < target:
+            start_index = middle_index + 1
+            middle_index = math.ceil((start_index + end_index) / 2)
     return -1  # not found
